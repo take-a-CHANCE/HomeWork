7efeaa6e70c94c3872b95f18facd29f1a9191444
take-a-CHANCE <chancejd53@gmail.com>
2015-02-17 10:06:00 -0500

    Added sorting methods for homework 2/17

--- Homework/src/HelloWorld.java
+++ Homework/src/HelloWorld.java
@@ -1,10 +1,13 @@
+import java.util.Comparator;
+
+import components.queue.Queue;
 import components.simplewriter.SimpleWriter;
 import components.simplewriter.SimpleWriter1L;
 
 /**
  * Simple HelloWorld program (clear of Checkstyle and FindBugs warnings).
- * 
- * @author P. Bucci
+ *
+ * @author Chance Davis
  */
 public final class HelloWorld {
 
@@ -17,7 +20,7 @@ public final class HelloWorld {
 
     /**
      * Main method.
-     * 
+     *
      * @param args
      *            the command line arguments; unused here
      */
@@ -27,4 +30,65 @@ public final class HelloWorld {
         out.close();
     }
 
+    /**
+     * Inserts the given {@code T} in the {@code Queue<T>} sorted according to
+     * the given {@code Comparator<T>} and maintains the {@code Queue<T>}
+     * sorted.
+     *
+     * @param <T>
+     *            type of {@code Queue} entries
+     * @param q
+     *            the {@code Queue} to insert into
+     * @param x
+     *            the {@code T} to insert
+     * @param order
+     *            the {@code Comparator} defining the order for {@code T}
+     * @updates q
+     * @requires <pre>
+     * IS_TOTAL_PREORDER([relation computed by order.compare method])  and
+     * IS_SORTED(q, [relation computed by order.compare method])
+     * </pre>
+     * @ensures <pre>
+     * perms(q, #q * <x>)  and
+     * IS_SORTED(q, [relation computed by order.compare method])
+     * </pre>
+     */
+    private static <T> void insertInOrder(Queue<T> q, T x, Comparator<T> order) {
+        boolean sorted = false;
+        Queue<T> holder = q.newInstance();
+        while (!sorted) {
+            T deq = q.dequeue();
+            int z = compare(deq, x);
+            if (z > 0) {
+                q.replaceFront(deq);
+                q.replaceFront(x);
+                sorted = true;
+            } else {
+                holder.enqueue(deq);
+            }
+        }
+        holder.append(q);
+        q.transferFrom(holder);
+    }
+
+    /**
+     * Sorts {@code this} according to the ordering provided by the
+     * {@code compare} method from {@code order}.
+     *
+     * @param order
+     *            ordering by which to sort
+     * @updates this
+     * @requires IS_TOTAL_PREORDER([relation computed by order.compare method])
+     * @ensures <pre>
+     * perms(this, #this)  and
+     * IS_SORTED(this, [relation computed by order.compare method])
+     * </pre>
+     */
+    public void sort(Comparator<T> order) {
+        Queue<T> sorted = this.newInstance();
+        while (this.size() > 0) {
+            insertInOrder(sorted, this.dequeue(), order);
+        }
+        this.transferFrom(sorted);
+    }
 }
